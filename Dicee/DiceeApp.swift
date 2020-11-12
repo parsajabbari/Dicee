e98de8054b6e836f17f9e74df20a134fd042d7a3
Parsa Jabbari <parsajabbari@gmail.com>
2020-11-12 00:43:38 -0800

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Parsa Jabbari on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
